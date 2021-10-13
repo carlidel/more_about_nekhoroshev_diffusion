ca3afa80abd6d785800bd639c999a2c76460dad6
Carlo Emilio Montanari <carlidel95@gmail.com>
2021-10-13 11:58:05 +0200

    missing files

--- working_experiment_script/list_files.py
+++ working_experiment_script/list_files.py
@@ -3,7 +3,10 @@ import numpy as np
 
 PATH = "/eos/project-d/da-and-diffusion-studies/Diffusion_Studies/new_games_with_diffusion/data"
 
+CHECK_PRESENCE = True
+
 files = list(filter(lambda x: "working" in x, os.listdir(PATH)))
+all_files = os.listdir(PATH)
 
 low_bound = [0.5, 0.1, 0.01, 0.001]
 high_bound = [np.inf, 1.0]
@@ -13,8 +16,51 @@ with open("file_list.txt", 'w') as out:
         print(os.path.join(PATH, f))
         for l in low_bound:
             for bb, fb in [(True, True), (True, False), (False, True)]:
+                
+                if fb and bb:
+                    method = "all"
+                elif fb and not bb:
+                    method = "forward_only"
+                elif bb and not fb:
+                    method = "backward_only"
+                else:
+                    method = "error"
+                    assert(False)
+
                 if bb:
                     for h in high_bound:
+                        if CHECK_PRESENCE:
+                            outname = "FIT_ub_{}_lb_{}_md_{}_".format(
+                                h, l, method) + f.replace("working_experiment_", "")
+                            if outname in all_files:
+                                flag = False
+                            else:
+                                flag = True
+                        else:
+                            flag = True
+                        
+                        if flag:
+                            out.write(
+                                str(os.path.join(PATH, f)) + " " +
+                                str(h) + " " +
+                                str(l) + " " +
+                                ("--backward " if bb else "--no-backward ") +
+                                ("--forward " if fb else "--no-forward ") +
+                                "\n"
+                            )
+                else:
+                    h=np.inf
+
+                    if CHECK_PRESENCE:
+                        outname = "FIT_ub_{}_lb_{}_md_{}_".format(h, l, method) + f.replace("working_experiment_", "")
+                        if outname in all_files:
+                            flag = False
+                        else:
+                            flag = True
+                    else:
+                        flag = True
+
+                    if flag:
                         out.write(
                             str(os.path.join(PATH, f)) + " " +
                             str(h) + " " +
@@ -23,14 +69,4 @@ with open("file_list.txt", 'w') as out:
                             ("--forward " if fb else "--no-forward ") +
                             "\n"
                         )
-                else:
-                    h=np.inf
-                    out.write(
-                        str(os.path.join(PATH, f)) + " " +
-                        str(h) + " " +
-                        str(l) + " " +
-                        ("--backward " if bb else "--no-backward ") +
-                        ("--forward " if fb else "--no-forward ") +
-                        "\n"
-                    )
 
